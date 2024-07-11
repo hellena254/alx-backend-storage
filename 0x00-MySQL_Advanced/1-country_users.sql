3803e4c0c5d163122129bb3130ffc8b181c31fad
hellena254 <hellenah243@gmail.com>
2024-07-11 06:11:49 -0700

    use ENUM data type

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- an SQL script that creates a table Users
+-- add ENUM data type
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+	)
